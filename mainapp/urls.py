00eb72ece7a399b54484cc2cfa7b14fe8693ecf7
Joyanta <joyanta@Joyantas-MacBook-Pro.local>
2022-04-15 20:27:53 +0600

    delete transaction, login, logout added

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -3,10 +3,18 @@ from django.urls import path
 # from . import views
 from .views import *
 
+from django.contrib.auth.views import LogoutView
+
 urlpatterns = [
+    path('login/', Login.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
     path('', Index.as_view(), name='index'),
     path('transaction/<int:pk>', TransactionDetail.as_view(),
          name='transaction_detail'),
     path('transaction-create/', TransactionCreate.as_view(),
          name='transaction-create'),
+    path('transaction-update/<int:pk>', TransactionUpdate.as_view(),
+         name='transaction-update'),
+    path('transaction-delete/<int:pk>', TransactionDelete.as_view(),
+         name='transaction-delete'),
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -4,12 +4,26 @@ from django.shortcuts import render, redirect
 from .models import *
 from django.views.generic.list import ListView  # listing
 from django.views.generic.detail import DetailView  # detail viewing
-from django.views.generic.edit import CreateView  # creating
+# creating, updating/modify
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 # redirecting to another link after certain task
 from django.urls import reverse_lazy
 
+from django.contrib.auth.views import LoginView
 
-class Index(ListView):
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+
+class Login(LoginView):
+    template_name = 'mainapp/login.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('index')
+
+
+class Index(LoginRequiredMixin, ListView):
     model = Finance  # database
     context_object_name = 'transactions'  # context object inside the html
     template_name = 'mainapp/index.html'  # specifying template name
@@ -25,10 +39,23 @@ class TransactionCreate(CreateView):
     model = Finance
     # fields = ['status', 'remarks'] #fixed fields
     fields = '__all__'  # all fields
-    success_url = reverse_lazy('index') #redirect after successful creation
+    success_url = reverse_lazy('index')  # redirect after successful creation
     context_object_name = 'transaction_create'
-    template_name = 'mainapp/transaction_create.html'
+    template_name = 'mainapp/transaction_form.html'
 
+
+class TransactionUpdate(UpdateView):
+    model = Finance
+    fields = '__all__'  # all fields
+    success_url = reverse_lazy('index')  # redirect after successful creation
+    template_name = 'mainapp/transaction_form.html'
+
+
+class TransactionDelete(DeleteView):
+    model = Finance
+    context_object_name = 'transaction'
+    success_url = reverse_lazy('index')  # redirect after successful creation
+    # template_name = 'mainapp/transaction_form.html'
 
 # def index(request):
 #     return HttpResponse("Hello, world. You're at the polls index.")
