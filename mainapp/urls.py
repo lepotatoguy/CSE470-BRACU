de583c161730cfef968633242b78f3e259ba4cc3
Joyanta <joyanta.jyoti.mondal@g.bracu.ac.bd>
2022-04-15 23:58:12 +0600

    register added

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -8,6 +8,7 @@ from django.contrib.auth.views import LogoutView
 urlpatterns = [
     path('login/', Login.as_view(), name='login'),
     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+    path('register/', Register.as_view(), name='register'),
     path('', Index.as_view(), name='index'),
     path('transaction/<int:pk>', TransactionDetail.as_view(),
          name='transaction_detail'),
--- mainapp/views.py
+++ mainapp/views.py
@@ -5,13 +5,15 @@ from .models import *
 from django.views.generic.list import ListView  # listing
 from django.views.generic.detail import DetailView  # detail viewing
 # creating, updating/modify
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
 # redirecting to another link after certain task
 from django.urls import reverse_lazy
 
-from django.contrib.auth.views import LoginView
-
+from django.contrib.auth.views import LoginView  # for login page
+# works to make sure if logged in properly to restrict user-based access
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
 
 class Login(LoginView):
@@ -23,10 +25,37 @@ class Login(LoginView):
         return reverse_lazy('index')
 
 
+class Register(FormView):
+    template_name = 'mainapp/register.html'
+    form_class = UserCreationForm
+    redirect_authenticated_user = True
+
+    success_url = reverse_lazy('index')
+
+    def form_valid(self, form):
+        user = form.save()
+        if user is not None:
+            login(self.request, user)
+        return super(Register, self).form_valid(form)
+
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('index')
+        return super(Register, self).get(*args, **kwargs)
+
+
 class Index(LoginRequiredMixin, ListView):
     model = Finance  # database
     context_object_name = 'transactions'  # context object inside the html
     template_name = 'mainapp/index.html'  # specifying template name
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['transactions'] = context['transactions'].filter(
+            user=self.request.user)  # total transactions for a user
+        context['count'] = context['transactions'].filter(
+            card_due=False).count()  # card due transactionss
+        return context
 
 
 class TransactionDetail(DetailView):
@@ -37,16 +66,21 @@ class TransactionDetail(DetailView):
 
 class TransactionCreate(CreateView):
     model = Finance
-    # fields = ['status', 'remarks'] #fixed fields
-    fields = '__all__'  # all fields
+    fields = ['status', 'remarks']  # fixed fields
+    # fields = '__all__'  # all fields
     success_url = reverse_lazy('index')  # redirect after successful creation
     context_object_name = 'transaction_create'
     template_name = 'mainapp/transaction_form.html'
 
+    def form_invalid(self, form):
+        form.instance.user = self.request.user
+        return super(TransactionCreate, self).form_valid(form)
+
 
 class TransactionUpdate(UpdateView):
     model = Finance
-    fields = '__all__'  # all fields
+    # fields = '__all__'  # all fields
+    fields = ['status', 'remarks']  # fixed fields
     success_url = reverse_lazy('index')  # redirect after successful creation
     template_name = 'mainapp/transaction_form.html'
 
